7e3b46b072a6721b6c0b285020ed2aaf8279e707
robiscjuan <robiscjuan1994@gmail.com>
2015-04-19 06:25:49 +0200

    version 0.2 -No se permite el modo horizontal (Manifest) -Nuevo login activity -Modularización de las clases--> complejidad a la hora de conectar entre ellas -Creación del row_follow, aún por terminar.

--- app/src/main/java/com/example/juan/aplicaciontwitter/MainActivity.java
+++ app/src/main/java/com/example/juan/aplicaciontwitter/MainActivity.java
@@ -6,6 +6,7 @@ import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import io.fabric.sdk.android.Fabric;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Locale;
 
@@ -40,10 +41,8 @@ import com.twitter.sdk.android.tweetui.TweetView;
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "cPhTnLOjY86H3BxhtAcHIBbdj";
-    private static final String TWITTER_SECRET = "X5DaymV0JXuzGpykzhK0In4uieTk6B8s5A1rqaM0oUUmKibZml";
-
-
+    private static final String TWITTER_KEY = "YkSUSp1BiT8Bk7MydRsoKhtdT";
+    private static final String TWITTER_SECRET = "zhHUz6LbS09dXIXPBGkRi659uErRIUnfpBJvQggqbV9OQPs0uk";
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -54,93 +53,80 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
     private TwitterLoginButton loginButton;
+
+    //TODO fijarse en el Bundle que se pasa al onCreate, nos servirá para tener la instancia cuando la memoria limpie
+    //Para saber sobre la gestión de Activities  http://expertojava.ua.es/dadm/restringido/android/sesion02-apuntes.html
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
-        setContentView(R.layout.login);
-        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
-        loginButton.setCallback(new Callback<TwitterSession>() {
+        // Do something with result, which provides a TwitterSession for making API calls
+        setContentView(R.layout.activity_main);
+        // Set up the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
-            public void success(Result<TwitterSession> result) {
-                // Do something with result, which provides a TwitterSession for making API calls
-                setContentView(R.layout.activity_main);
-                // Set up the action bar.
-                final ActionBar actionBar = getSupportActionBar();
-                actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-                // Create the adapter that will return a fragment for each of the three
-                // primary sections of the activity.
-                mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-                // Set up the ViewPager with the sections adapter.
-                mViewPager = (ViewPager) findViewById(R.id.pager);
-                mViewPager.setAdapter(mSectionsPagerAdapter);
-
-                // When swiping between different sections, select the corresponding
-                // tab. We can also use ActionBar.Tab#select() to do this if we have
-                // a reference to the Tab.
-                mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-                    @Override
-                    public void onPageSelected(int position) {
-                        actionBar.setSelectedNavigationItem(position);
-                    }
-                });
-
-                // For each of the sections in the app, add a tab to the action bar.
-                for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-                    // Create a tab with text corresponding to the page title defined by
-                    // the adapter. Also specify this Activity object, which implements
-                    // the TabListener interface, as the callback (listener) for when
-                    // this tab is selected.
-                    actionBar.addTab(
-                            actionBar.newTab()
-                                    .setText(mSectionsPagerAdapter.getPageTitle(i))
-                                    .setTabListener(MainActivity.this));
-                }
-
-                //Después de la creación de tabs
-
-                TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
-// Can also use Twitter directly: Twitter.getApiClient()
-                StatusesService statusesService = twitterApiClient.getStatusesService();
-                statusesService.homeTimeline(10,null,null,null,null,null,false,new Callback<List<Tweet>>() {
-                    @Override
-                    public void success(Result<List<Tweet>> listResult) {
-                        //ListView listTweets = (ListView)  mSectionsPagerAdapter.getRegistrerFragment(mViewPager.getCurrentItem().findViewById(R.id.listTweets);
-
-                        ListView listTweets = (ListView)  mSectionsPagerAdapter.getRegistrerFragment(mViewPager.getCurrentItem()).getView().findViewById(R.id.listTweets);
-                        listTweets.setAdapter(new CustomTweetAdapter(getApplicationContext(), R.layout.row_tweet, listResult.data));
-                    }
-
-                    @Override
-                    public void failure(TwitterException e) {
-
-                    }
-                });
-
-            }
-
-            @Override
-            public void failure(TwitterException exception) {
-                // Do something on failure
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
             }
         });
 
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(MainActivity.this));
+        }
+
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this,new Twitter(authConfig));
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, 1);
+
+
+
 
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        loginButton.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            //TODO después de muchas pruebas los listview fuera del callback se rompen, no se aún por qué
+            //TODO en teoría habría que comprobar que el resultCode es correcto, pero si lo hago no entra nunca, habrá que saber como acabar una ejecución para que sea correcta
+          //  ListView listTweets = (ListView)  mSectionsPagerAdapter.getRegistrerFragment(mViewPager.getCurrentItem()).getView().findViewById(R.id.listTweets);
+                TweetActivity tweetActivity = new TweetActivity();
+                tweetActivity.obtenerListadoDeTweets(getApplicationContext(),mViewPager,mSectionsPagerAdapter);
+            }
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -180,16 +166,51 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
     }
 
     /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            return rootView;
+        }
+    }
+
+    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
+        private final int NUMEROTABS = 3;
+        private PlaceholderFragment[] arrayFragment = new PlaceholderFragment[NUMEROTABS];
+        private int numFragment;
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
-        private PlaceholderFragment[] arrayFragment = new PlaceholderFragment[3];
-        private int numFragment;
+
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
@@ -202,7 +223,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return NUMEROTABS;
         }
 
         @Override
@@ -220,41 +241,8 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
 
         public Fragment getRegistrerFragment(int position){
+
             return arrayFragment[position];
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            return rootView;
         }
     }
 
